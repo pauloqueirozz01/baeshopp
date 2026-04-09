941e5c021fd55a4ddd0a7084deb416c9e6294e70
Paulo Queiroz | IOS Dev <pauloqueirozzdev@gmail.com>
2026-04-09 16:45:14 -0300

    feat: adicionando FlatList para renderizar os items do Array

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,11 @@
-import { View, Image, TouchableOpacity, Text } from "react-native";
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 import { styles } from "./styles";
 
@@ -9,8 +16,10 @@ import { FilterStatus } from "@/types/FilterStatus";
 import Item from "@/components/Item";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.DONE, FilterStatus.PENDING];
+const ITEMS = Array.from({ length: 100 }).map((_, index) => String(index));
 
 export function Home() {
+  console.log("ITEMS: ", ITEMS);
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
@@ -29,10 +38,26 @@ export function Home() {
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
-        <Item
-          data={{ status: FilterStatus.DONE, description: "Café" }}
-          onStatus={() => console.log("Status alterado")}
-          onRemove={() => console.log("Item Removido")}
+        {/* <ScrollView>
+          {ITEMS.map((value) => (
+            <Item
+              key={value}
+              data={{ status: FilterStatus.DONE, description: "Café" }}
+              onStatus={() => console.log("Status alterado")}
+              onRemove={() => console.log("Item Removido")}
+            />
+          ))}
+        </ScrollView> */}
+        <FlatList
+          data={ITEMS}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Item
+              data={{ status: FilterStatus.DONE, description: item }}
+              onStatus={() => console.log("Status alterado")}
+              onRemove={() => console.log("Item Removido")}
+            />
+          )}
         />
       </View>
     </View>
