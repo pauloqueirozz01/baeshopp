19c5e501189f335d5a8bf881c0dd7df340e36be1
Paulo Queiroz | IOS Dev <pauloqueirozzdev@gmail.com>
2026-04-09 13:52:37 -0300

    adicionando um bottomSheet header para alinhas status do produto, junto com um botão de limpar os filtros

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,12 +1,13 @@
-import { View, Image } from "react-native";
+import { View, Image, TouchableOpacity, Text } from "react-native";
 
 import { styles } from "./styles";
 
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { Filter } from "@/components/Filter";
-
 import { FilterStatus } from "@/types/FilterStatus";
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.DONE, FilterStatus.PENDING];
 
 export function Home() {
   return (
@@ -19,8 +20,14 @@ export function Home() {
       </View>
 
       <View style={styles.content}>
-        <Filter status={FilterStatus.DONE} isActive />
-        <Filter status={FilterStatus.PENDING} isActive={false} />
+        <View style={styles.header}>
+          {FILTER_STATUS.map((status) => (
+            <Filter key={status} status={status} isActive />
+          ))}
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
